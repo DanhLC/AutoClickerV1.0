613475fa3a1075db48981598fb628b05e3e11de5
Ryo3504 <megafan7@gmail.com>
2023-12-03 15:45:51 +0700

    init autoclicker

---
+++ Autoclicker.py
@@ -0,0 +1,31 @@
+import pyautogui
+import threading
+import keyboard
+
+# Đặt thời gian trễ giữa các lần click thành 0.001 giây (1ms)
+pyautogui.PAUSE = 0.001
+
+def click():
+    while True:
+        if stop_clicking:
+            break
+        pyautogui.click()
+
+def start_auto_click():
+    global stop_clicking
+    stop_clicking = False
+    threading.Thread(target=click).start()
+
+def stop_auto_click():
+    global stop_clicking
+    stop_clicking = True
+
+keyboard.add_hotkey('f4', start_auto_click)
+keyboard.add_hotkey('f5', stop_auto_click)
+
+# Khởi tạo biến để dừng auto-click
+stop_clicking = True
+
+# Bắt đầu vòng lặp chính để duy trì việc chạy mã
+while True:
+    pass
